6b2671c9430e873e5c687a8d6dfb49ca43be493f
2200159542 <91772158+2200159542@users.noreply.github.com>
2022-08-19 21:13:09 +0800

    Add files via upload

---
+++ include/Socket.h
@@ -0,0 +1,38 @@
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+
+#define BUFSIZE 20
+
+
+
+class Socket{
+
+protected:
+	const char* ip;
+	unsigned short int port;
+	int fd;
+	char buf[BUFSIZE];
+	int magsize;
+	struct sockaddr_in addr;
+	
+	void conv();
+
+public:
+
+	Socket(const char* ip, unsigned short int port);
+	Socket(unsigned short int port);
+	~Socket();
+	
+	int link();
+	char* getbuf(){return this->buf;}
+//	virtual int Send(const char* s)=0;
+//	virtual int Receive()=0;
+//	virtual void Print()=0;
+//	virtual int Send(const char* s,int i)=0;
+//	virtual int Receive(int i)=0;
+//	virtual void Print(int i)=0;
+//	virtual int setmagsize(int i)=0;
+	
+};
